0e154981ac7802b3beb3aa64615efce0365bd0c6
arioum <amoghrao121@gmail.com>
2023-07-03 18:22:59 +0530

    layout fix + form enhancements

--- client/src/components/ReservationForm/ReservationForm.jsx
+++ client/src/components/ReservationForm/ReservationForm.jsx
@@ -1,12 +1,48 @@
+import { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+import classes from './reservationForm.module.scss'
+
 const ReservationForm = () => {
-  const isOpen = true;
+  const [isOpen, setIsOpen] = useState(true);
+
+  // setInterval(() => {
+  //   getTime()
+  // }, 60000)
+
+  const getTime = () => {
+    const time = new Date().toLocaleTimeString().split(" ");
+    const modTime = time[0].split(":");
+    console.log(modTime);
+  }
+  getTime();
+
+  useEffect(() => {
+  }, [])
 
   return (
-    <form>
+    <form className={classes.form}>
+      <h1>Quick Reserve</h1>
       {isOpen ? (
-        <div className="">
+        <div className={classes.form__inputContainer}>
+          <div className={classes.form__inputContainer__group}>
+            <label htmlFor="">Table for</label>
+            <select name="" id="">
+              <option value="">1</option>
+            </select>
+          </div>
+
+          <div className={classes.form__inputContainer__group}>
+            <label htmlFor="">Table for</label>
+            <select name="" id="">
+              <option value="">1</option>
+            </select>
+          </div>
+
           <label htmlFor="">Table for</label>
-          <input type="dropdown" />
+          <input type="radio" />
+          <input type="radio" />
+
+          <Button>Next</Button>
         </div>
       ) : null}
     </form>
