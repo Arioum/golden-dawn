4192c46bf3fd0420271a031044572f2b6d5e291f
arioum <amoghrao121@gmail.com>
2023-12-07 23:16:42 +0530

    added admin page and menu page

---
+++ client/src/pages/Admin/AdminPage.jsx
@@ -0,0 +1,141 @@
+import { useState } from 'react';
+import axios from 'axios';
+import HeaderLayout from '../../layouts/Header/HeaderLayout';
+import Button from '../../components/Button/Button';
+
+const AdminPage = () => {
+  const [menuItem, setMenuItem] = useState({
+    itemName: '',
+    price: 0,
+    discount: 0,
+    description: '',
+    images: [],
+    ratings: 0,
+    totalOrdered: 0,
+    category: '',
+    tags: [],
+  });
+
+  function onFormChange(e) {
+    const { value, name } = e.target;
+
+    if (name === 'images') {
+      const files = Array.from(e.target.files);
+      setMenuItem((prev) => ({
+        ...prev,
+        [name]: files,
+      }));
+    } else {
+      setMenuItem((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+    e.preventDefault();
+  }
+
+  async function createMenuItem(e) {
+    const formData = new FormData();
+
+    Object.entries(menuItem).forEach(([key, value]) => {
+      if (key === 'images') {
+        value.forEach((file) => {
+          formData.append('images', file);
+        });
+      } else {
+        formData.append(key, value);
+      }
+    });
+    console.log(formData);
+    axios
+      .post('http://localhost:4000/createItem', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .catch((err) => console.log(`Error occured: ${err}`));
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <HeaderLayout
+        title={'Add new Menu Item'}
+        subTitle={'Carefully enter item details'}
+      >
+        <Button></Button>
+      </HeaderLayout>
+      <form className='container' onSubmit={createMenuItem}>
+        <div className=''>
+          <label htmlFor=''>Item Name</label>
+          <input
+            type='text'
+            name='itemName'
+            value={menuItem.name}
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className=''>
+          <label htmlFor=''>Price</label>
+          <input
+            type='number'
+            name='price'
+            value={menuItem.price}
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className=''>
+          <label htmlFor=''>Discount</label>
+          <input
+            type='number'
+            name='discount'
+            value={menuItem.discount}
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className=''>
+          <label htmlFor=''>Item Description</label>
+          <input
+            type='text'
+            name='description'
+            value={menuItem.description}
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className=''>
+          <label htmlFor=''>Images</label>
+          <input
+            type='file'
+            name='images'
+            multiple
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+
+        <div className=''>
+          <label htmlFor=''>Category</label>
+          <input
+            type='text'
+            name='category'
+            value={menuItem.category}
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className=''>
+          <label htmlFor=''>Tags</label>
+          <input
+            type='text'
+            name='tags'
+            value={menuItem.tags}
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className=''>
+          <input type='submit' value={'Create Item'} />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default AdminPage;
--- client/src/pages/Menu/MenuPage.jsx
+++ client/src/pages/Menu/MenuPage.jsx
@@ -1,11 +1,39 @@
-import HeaderLayout from "../../layouts/Header/HeaderLayout"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import HeaderLayout from '../../layouts/Header/HeaderLayout';
+import MenuView from '../../layouts/MenuView/MenuView';
+import classes from './menuPage.module.scss';
 
 const MenuPage = () => {
+  const [menuItems, setMenuItems] = useState(null);
+
+  useEffect(() => {
+    const fetchMenuItems = async () => {
+      axios
+        .get('http://localhost:4000/getMenuItems')
+        .then((res) => setMenuItems(res.data))
+        .catch((err) => console.log('error loading menu items: ' + err));
+    };
+    fetchMenuItems();
+  }, []);
+
+  {
+    /* {item.images.map((img) => (
+    <img src={item.images[0].url} alt='No image' width={300} />
+  ))} */
+  }
   return (
     <>
-      <HeaderLayout title={"Menu"}/>
+      <HeaderLayout title={'Menu'} />
+      <main className={`container ${classes.menuMain}`}>
+        {menuItems ? (
+          <MenuView menuItems={menuItems} />
+        ) : (
+          <p className={classes.menuMain__loading}>Loading Menu...</p>
+        )}
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default MenuPage
+export default MenuPage;
