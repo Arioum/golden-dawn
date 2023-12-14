373df69ff110c74d13bae2ea65f6b34db9bc6ae9
arioum <amoghrao121@gmail.com>
2023-12-14 23:54:55 +0530

    added toggle button and modified sidebar logic

--- client/src/layouts/MenuView/MenuSidebar.jsx
+++ client/src/layouts/MenuView/MenuSidebar.jsx
@@ -1,19 +1,59 @@
 import classes from './menuView.module.scss';
 
-const MenuSidebar = () => {
+const MenuSidebar = ({ menuItems, scrollToCategory }) => {
+  const categories = [
+    'Starters',
+    'Rice Preparation',
+    'Biriyani',
+    'Bread',
+    'Main Course',
+    "Pizza's and Pasta",
+    'Dessert',
+    'Ice creams',
+    'Beverages',
+  ];
+
   return (
     <section className={classes.menuSidebar}>
       <ul>
-        <li><button>Starters</button></li>
-        <li><button>Rice preparation</button></li>
-        <li><button>Biriyani</button></li>
-        <li><button>Bread</button></li>
-        <li><button>Main course</button></li>
-        <li><button>Beverages</button></li>
-        <li><button>Pizza's and Pasta</button></li>
+        {menuItems.map((category, index) => (
+          <li key={index}>
+            <button
+              className={classes.menuSidebar__button}
+              onClick={() => scrollToCategory(category._id)}
+            >
+              <span>{category._id}</span>
+              <span>{category.itemsCount}</span>
+            </button>
+          </li>
+        ))}
+        <li>
+          <button>Rice preparation</button>
+        </li>
+        <li>
+          <button>Biriyani</button>
+        </li>
+        <li>
+          <button>Bread</button>
+        </li>
+        <li>
+          <button>Main course</button>
+        </li>
+        <li>
+          <button>Pizza's and Pasta</button>
+        </li>
+        <li>
+          <button>Dessert</button>
+        </li>
+        <li>
+          <button>Ice creams</button>
+        </li>
+        <li>
+          <button>Beverages</button>
+        </li>
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export default MenuSidebar
+export default MenuSidebar;
